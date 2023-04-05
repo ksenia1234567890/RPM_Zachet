8f17d006ea2d95630d5604c5ecf3adb47aaf0dc8
student <student@1405-02>
2023-04-05 17:41:34 +0300

    Добавьте файлы проекта.

---
+++ рпм_зачёт/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace рпм_зачёт
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(149, 129);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(461, 59);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(303, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите слово";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(159, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(451, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Если ввести больше одного слова, программа не будет выполнена";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(307, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 27);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Узнать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 269);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Зачётное задание";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ рпм_зачёт/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace рпм_зачёт
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Событие определяет, является ли слово палиндромомом
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string word = textBox1.Text;
+            string thing = "";
+            string d1 = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnmЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮйцукенгшщзхъфывапролджэячсмитьбю";
+            
+            List<char> l = new List<char>();
+
+            // Проверка - слово или нет
+            string answer1 = Slovo(word, d1);
+
+                    // Если строка является словом
+
+                    if(answer1 == "да")
+                    {
+                        // Запись букв
+
+                        for (int m = 0; m < word.Length; m++)
+                        {
+                            l.Add(word[m]);
+                        }
+                        l.Reverse();
+                        for (int n = 0; n < word.Length; n++)
+                        {
+                            thing += l[n];
+                        }
+
+                // Если первое слово равно второму, слово является палиндромомом. Если нет - слово не является палиндромомом
+
+                string w_l = word.ToLower();
+                string t_l = thing.ToLower();
+
+                if (w_l==t_l)
+                        {
+                            textBox1.Text = "Слово является палиндромомом";
+                        }
+                        else
+                        {
+                            textBox1.Text = "Слово не является палиндромомом";
+                        }
+                    }
+
+                    // Строка не вляется словом
+
+                    else
+                     {
+                        textBox1.Text = "Строка не является словом";
+                     }
+        }
+
+        public string Slovo(string word, string d)
+        {
+            
+            for (int i = 0; i < d.Length; i++)
+            {
+                for (int a = 0; a <word.Length; a++)
+                {
+
+                    if (d.Contains(word[a]) == false)
+                    {
+                        return "нет";
+                    }
+                }
+            }
+            return "да";
+        }
+    }
+}
